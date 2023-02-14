3ff1b15392cc53c3f97fb025ee6007f101d30702
chukhranchick <ivan.chuhran@gmail.com>
2023-02-14 12:33:02 +0200

    change maxpool kernel size

--- models.py
+++ models.py
@@ -1,14 +1,8 @@
-import datetime
-import gc
-import os
-from typing import Optional, Union
+from typing import Union
 
+import numpy as np
 import torch
 from torch import nn
-from torch.cuda.amp import autocast
-from torch.utils.data import DataLoader
-from tqdm.auto import tqdm
-import numpy as np
 
 
 def eval_shape(height, width, model, pool=0):
@@ -24,126 +18,10 @@ def eval_shape(height, width, model, pool=0):
             elif isinstance(layer, nn.MaxPool2d):
                 height = np.floor((height + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1)
                 width = np.floor((width + 2 * padding - dilation * (kernel_size - 1) - 1) / stride + 1)
-        print(height, width)
     if pool:
         height /= pool
         width /= pool
     return int(height), int(width)
-
-
-def one_epoch(
-        model: nn.Module,
-        dataloader: DataLoader,
-        criterion: nn.Module,
-        optimizer=None,
-        device: str = 'cpu'):
-    running_loss = 0.0
-    running_accuracy = 0.0
-    number_of_samples = 0
-    for i, data in enumerate(tqdm(dataloader, desc='Batches', position=0, leave=False)):
-        x, y = data
-        x = x.half().to(device)
-        y = y.to(device).float()
-
-        sample_size = x.size(0)
-
-        if optimizer:
-            optimizer.zero_grad()
-
-        with autocast():
-            output = model(x).view(len(x))
-            loss = criterion(output, y)
-        output = torch.sigmoid(output)
-        y = y.int()
-        output = (output >= 0.5) * 1
-
-        running_accuracy += torch.sum(output == y).item()
-        running_loss += float(loss.item() * sample_size)
-        number_of_samples += sample_size
-
-        if optimizer:
-            loss.backward()
-            optimizer.step()
-
-        if torch.cuda.is_available():
-            del x, output, loss
-            gc.collect()
-            with torch.cuda.device(device):
-                torch.cuda.empty_cache()
-
-    loss = running_loss / len(dataloader.dataset)
-    accuracy = running_accuracy / number_of_samples
-    return loss, accuracy
-
-
-def train(
-        model: nn.Module,
-        dataloaders: dict[str, DataLoader],
-        criterion: nn.Module,
-        optimizer: torch.optim.Optimizer,
-        epochs: int = 50,
-        stats: dict = None,
-        device: str = 'cpu',
-        early_stopping: bool = True,
-        patience: int = 5) -> None:
-    current_date = datetime.datetime.now().strftime('%d.%m.%y')
-    model_name = model.__class__.__name__
-    save_dir = os.path.join('weights', current_date, model_name)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    save_dir = os.path.join(save_dir, f'{model_name}.pt')
-    early_stopping_counter: int = 0
-    for epoch in range(epochs):
-        print(f"epoch {epoch}/{epochs}")
-        avg_train_loss, avg_train_accuracy = one_epoch(model, dataloaders['train'], criterion, optimizer, device)
-
-        with torch.no_grad():
-            avg_valid_loss, avg_valid_accuracy = one_epoch(model, dataloaders['validation'], criterion, device=device)
-
-        if early_stopping:
-            if stats['validation']['loss'] and stats['train']['loss']:
-                if (avg_valid_loss > stats['validation']['loss'][-(1 + early_stopping_counter)] or
-                        avg_train_loss > stats['train']['loss'][-(1 + early_stopping_counter)]):
-                    early_stopping_counter += 1
-                    if early_stopping_counter == patience:
-                        print("Early Stopping...")
-                        return
-                else:
-                    early_stopping_counter = 0
-
-        if stats['validation']['loss'] and stats['train']['loss']:
-            if (avg_valid_loss < stats['validation']['loss'][-(1 + early_stopping_counter)] or
-                    avg_train_loss < stats['train']['loss'][-(1 + early_stopping_counter)]):
-                save_state(model, optimizer, save_dir)
-
-        stats['train']['loss'].append(avg_train_loss)
-        stats['train']['accuracy'].append(avg_train_accuracy)
-
-        stats['validation']['loss'].append(avg_valid_loss)
-        stats['validation']['accuracy'].append(avg_valid_accuracy)
-
-        print(f"\nTraining average loss - {avg_train_loss}, average accuracy - {avg_train_accuracy}")
-        print(f"Validation loss - {avg_valid_loss},  accuracy - {avg_valid_accuracy}")
-
-
-def save_state(model: nn.Module,
-               optimizer: torch.optim.Optimizer,
-               filename: str) -> None:
-    state_dict: dict = {
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict()
-    }
-    torch.save(state_dict, filename)
-
-
-def load_state(model: nn.Module,
-               optimizer: torch.optim.Optimizer,
-               filename: str) -> tuple[nn.Module, Optional[torch.optim.Optimizer]]:
-    state_dict = torch.load(filename)
-    model.load_state_dict(state_dict['model_state_dict'])
-    if optimizer:
-        optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-    return model, optimizer
 
 
 def init_weight(m):
@@ -229,27 +107,30 @@ class Conv4Net(nn.Module):
         super(Conv4Net, self).__init__()
         input_channels: int = params['in_channels']
         out_channels: int = params['out_channels']
-        image_size: tuple = params['image_size']
+        image_size: tuple = params['input_image_size']
 
         self.block = nn.Sequential(
             ConvBlock(input_channels, out_channels, 7),
-            nn.MaxPool2d(3),
+            nn.MaxPool2d(2),
             ConvBlock(out_channels, out_channels * 2, 5),
-            nn.MaxPool2d(3),
-            ConvBlock(out_channels * 2, out_channels * 4, 5),
+            nn.MaxPool2d(2),
+            ConvBlock(out_channels * 2, out_channels * 4),
             nn.MaxPool2d(2),
             ConvBlock(out_channels * 4, out_channels * 8),
             nn.MaxPool2d(2)
         )
         self.relu = nn.ReLU()
         h, w = eval_shape(*image_size, self.block)
+        print(f'image size after conv block - {h, w}')
         self.fc1 = nn.Linear(out_channels * 8 * h * w, 128)
         self.fc2 = nn.Linear(128, 1)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.block(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
